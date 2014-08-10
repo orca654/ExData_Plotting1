a12af9b0a2736ed77e3b10d3d2a87b4d6b1b6ec0
orca654 <b80602021@hotmail.com>
2014-08-10 16:15:46 -0700

    commint

--- con---file-household_power_consumption.txt-r-data---read.csv-con-as.Date-data-1-format-d/m/plot2.r
+++
@@ -1,3 +0,0 @@
-con <- file("household_power_consumption.txt", "r")
-data <- read.csv(con)
-as.Date(data[,1], format="%d/%m/%Y")
--- plot3.r
+++ plot3.r
@@ -1,3 +1,2 @@
 con <- file("household_power_consumption.txt", "r")
 data <- read.csv(con)
-as.Date(data[,1], format="%d/%m/%Y")
